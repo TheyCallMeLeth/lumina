33e956a651f28c2b86ce933587a1eee08c1135bf
TheyCallMeLeth <raullalvespe1227@gmail.com>
2026-05-30 12:42:28 -0300

    Novo design dashboard

--- src/main/java/br/com/lumina/controller/DashboardController.java
+++ src/main/java/br/com/lumina/controller/DashboardController.java
@@ -1,13 +1,34 @@
 package br.com.lumina.controller;
 
+import br.com.lumina.domain.usuario.Usuario;
+import br.com.lumina.domain.usuario.UsuarioRepository;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
 
+    private final UsuarioRepository repository;
+
+    public DashboardController(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping("/dashboard")
-    public String dashboard() {
+    public String dashboard(
+            Authentication authentication,
+            Model model) {
+
+        String email = authentication.getName();
+
+        Usuario usuario = repository
+                .findByEmail(email)
+                .orElseThrow();
+
+        model.addAttribute("usuario", usuario);
+
         return "dashboard/index";
     }
 }
